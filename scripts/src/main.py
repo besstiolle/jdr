3b9785c6fbfa19fd7ac596a6ead38059bb4b72b8
Kevin Danezis <contact@kdanezis.fr>
2023-06-05 21:22:18 +0200

    :sparkles: add script to generate AR item for inventory

--- scripts/src/main.py
+++ scripts/src/main.py
@@ -1,17 +1,21 @@
 
 print("Generation des pastilles 'Particularite'")
-with open("runnerParticularite.py") as f:
+with open("runnerParticularites.py") as f:
     exec(f.read())
 
 print("Generation des pastilles 'Enjeux'")
-with open("runnerEnjeu.py") as f:
+with open("runnerEnjeux.py") as f:
     exec(f.read())
 
 print("Generation des images 'Point d'Intérêt'")
-with open("runnerPointOfInteret.py") as f:
+with open("runnerPointsOfInteret.py") as f:
     exec(f.read())
 
 print("Generation des images 'Biomes'")
 with open("runnerBiomes.py") as f:
+    exec(f.read())
+
+print("Generation des images 'Items'")
+with open("runnerItems.py") as f:
     exec(f.read())
 
